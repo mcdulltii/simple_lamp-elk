c7a40867c71d360680d35b969d1761babbd26de3
root <root@localhost.localdomain>
2020-07-25 04:08:09 -0400

    Minor fixes

--- setup.sh
+++ setup.sh
@@ -17,4 +17,5 @@ CREATE TABLE `upload_images` (
 LOCK TABLES `upload_images` WRITE;
 INSERT INTO `upload_images` VALUES (1,'Qingye Jiang','clouds1.jpg','2015-01-31 04:21:11'),(2,'Qingye Jiang','clouds2.jpg','2015-01-31 04:21:15'),(3,'Qingye Jiang','clouds3.jpg','2015-01-31 04:21:20'),(4,'Qingye Jiang','clouds4.jpg','2015-01-31 04:21:25'),(5,'Qingye Jiang','clouds5.jpg','2015-01-31 04:24:26'),(6,'Qingye Jiang','clouds6.jpg','2015-01-31 04:24:30'),(7,'Qingye Jiang','clouds7.jpg','2015-01-31 04:24:34'),(8,'Qingye Jiang','clouds8.jpg','2015-01-31 04:24:38'),(9,'Qingye Jiang','clouds9.jpg','2015-01-31 04:25:49'),(10,'Qingye Jiang','clouds10.jpg','2015-01-31 04:25:53');
 UNLOCK TABLES;"
+sleep 2
 docker exec -it simple_lamp-elk_mysql_1 mysql -pPassw0rd!
